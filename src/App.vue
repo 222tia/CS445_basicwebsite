8a93275cb552b1067fc03c55ed6becbe3ebbaf21
Tia <112046117+222tia@users.noreply.github.com>
2025-02-06 16:09:58 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+  import { reactive, ref } from 'vue';
+
+  const items = reactive([
+    { name: "kewpie mayo", count: 0 },
+    { name: "furikake", count: 0 },
+    { name: "nori", count: 0 }
+  ]);
+
+  const message = ref("");
+
+  function increase(index) {
+    items[index].count++;
+  };
+
+  function decrease(index) {
+    if (items[index].count > 0) {
+      items[index].count--;
+    }
+  };
+
+  function showMessage() {
+    const messages = ["Order placed!", 
+                      "Thank you for your order!", 
+                      "No."];
+    const random = Math.floor(Math.random() * messages.length);
+    message.value = messages[random];
+  }
+
+</script>
+
+<template>
+
+<div>
+  <div>
+    <h1>Add to Order</h1> 
+  </div>
+
+  <div>
+    <div v-for="(item, index) in items" :key="item.name">
+      <p>{{ item.name }} 
+        <button @click="decrease(index)">-</button>
+        <button @click="increase(index)">+</button> 
+        : {{ item.count }}</p>
+    </div>
+    <div style="padding-top:10px">
+      <button @click="showMessage">order</button>
+      <p>{{ message }}</p>
+    </div>
+  </div>
+</div>
+
+</template>
+
+<style scoped>
+
+p {
+  padding-top: 10px;
+}
+
+</style>
